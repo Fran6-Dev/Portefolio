56ef4bdabd345de90fd9b81aa37bb30891b18dd1
Francis Lukoki <francislukoki@hotmail.fr>
2022-11-24 23:06:28 +0100

    portefolio v1

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Réseau social d'entreprise",
+    subtitle: "MERN Stack",
+    description:
+      "Réalisation d'un réseau social interne permettant de communiquer entre salarié pour l'entreprise Groupomania",
+    image: "./projet-1.gif",
+    link: "https://github.com/Fran6-Dev/Groupomania",
+  },
+  {
+    title: "Kanap",
+    subtitle: "Javascript",
+    description:
+      "Création d'un site e-commerce permettant d'acheter des canapés en ligne",
+    image: "./projet-3.gif",
+    link: "https://github.com/Fran6-Dev/Kanap",
+  },
+  {
+    title: "Booki",
+    subtitle: "Transformez une maquette en site web avec HTML & CSS",
+    description:
+      "Mise en application de mes connaissances en CSS pour transformer une maquette à l'identique",
+    image: "./projet-4.gif",
+    link: "https://github.com/Fran6-Dev/Booki",
+  },
+  {
+    title: "Ohmyfood",
+    subtitle: "Dynamiser une page web avec des animation CSS à l'aide de Sass",
+    description:
+      "Mettre en oeuvre des effets CSS graphique avancés",
+    image: "./projet-2.gif",
+    link: "https://github.com/Fran6-Dev/Ohmyfood",
+  },
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Redux",
+  "Express",
+  "MongoDB",
+  "Sass",
+];
